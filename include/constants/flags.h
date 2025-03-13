657b6f24690f8cb4714e18d4d7443e29a2de18f5
thalescd <thalescd96@gmail.com>
2025-03-13 10:03:22 -0300

    Map item change and Float Stone Guy added to Rustboro

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -46,7 +46,7 @@
 #define FLAG_EXP_SHARE_ACTIVE               0x20
 #define FLAG_RECEIVED_RUSTBORO_MART_ITEM    0x21
 #define FLAG_RECEIVED_SLATEPORT_HASTY_MINT  0x22
-#define FLAG_UNUSED_0x023                   0x23 // Unused Flag
+#define FLAG_RECEIVED_FLOAT_STONE_RUSTBORO  0x23
 #define FLAG_UNUSED_0x024                   0x24 // Unused Flag
 #define FLAG_UNUSED_0x025                   0x25 // Unused Flag
 #define FLAG_UNUSED_0x026                   0x26 // Unused Flag
