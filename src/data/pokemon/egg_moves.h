19db2df72c7eb7872f427bde0ea7362a8ffe1096
Thales Castro <thalescd96@gmail.com>
2025-10-16 17:25:44 -0300

    Updates

--- src/data/pokemon/egg_moves.h
+++ src/data/pokemon/egg_moves.h
@@ -63,7 +63,6 @@ static const u16 sSquirtleEggMoveLearnset[] = {
     MOVE_BRINE,
     MOVE_DRAGON_PULSE,
     MOVE_AURA_SPHERE,
-    MOVE_ORIGIN_PULSE,
     MOVE_UNAVAILABLE,
 };
 #endif //P_FAMILY_SQUIRTLE
