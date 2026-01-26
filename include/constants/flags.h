c7382ce161f327f374c841e441cac75214ba8ccf
Thales Castro <thalescd96@gmail.com>
2026-01-26 11:27:46 -0300

    Item Location and Norman changes

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1068,7 +1068,7 @@
 #define FLAG_ITEM_ROUTE_114_WATER_STONE                             0x3F7
 #define FLAG_ITEM_ROUTE_115_SUPER_POTION                            0x3F8
 #define FLAG_ITEM_ROUTE_115_TM_FOCUS_PUNCH                          0x3F9
-#define FLAG_ITEM_ROUTE_115_IRON                                    0x3FA
+#define FLAG_ITEM_ROUTE_115_ABILITY_CAPSULE                         0x3FA
 #define FLAG_ITEM_ROUTE_116_ETHER                                   0x3FB
 #define FLAG_ITEM_ROUTE_116_REPEL                                   0x3FC
 #define FLAG_ITEM_ROUTE_116_REVIVE                                  0x3FD
