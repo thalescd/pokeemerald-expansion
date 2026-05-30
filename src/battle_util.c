fa10d3a1593fd26a04b062b5a149827483720e3a
Thales Castro <thalescd96@gmail.com>
2026-05-30 17:39:37 -0300

    possible fix for intimidate weird interactions

--- src/battle_util.c
+++ src/battle_util.c
@@ -3313,6 +3313,7 @@ u32 AbilityBattleEffects(enum AbilityEffect caseID, enum BattlerId battler, enum
             {
                 gEffectBattler = battler;
                 gBattleStruct->intimidateActivated = TRUE;
+                ClearStatChangeValues();
                 for (enum BattlerId i = 0; i < gBattlersCount; i++)
                 {
                     if (IsBattlerAlly(battler, i) || !IsBattlerAlive(i))
@@ -3330,6 +3331,7 @@ u32 AbilityBattleEffects(enum AbilityEffect caseID, enum BattlerId battler, enum
             {
                 gEffectBattler = battler;
                 GetBattlerPartyState(battler)->supersweetSyrup = TRUE;
+                ClearStatChangeValues();
                 for (enum BattlerId i = 0; i < gBattlersCount; i++)
                 {
                     if (IsBattlerAlly(battler, i) || !IsBattlerAlive(i))
