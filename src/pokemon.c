da245ff5d3d42d422f16b667def8a1a1979c5a7f
Thales Castro <thalescd96@gmail.com>
2026-06-27 22:49:26 -0300

    increased wild mon held item chances

--- src/pokemon.c
+++ src/pokemon.c
@@ -5575,7 +5575,7 @@ void SetWildMonHeldItem(void)
         u16 i;
         bool32 itemHeldBoost = CanFirstMonBoostHeldItemRarity();
         u16 chanceNoItem = itemHeldBoost ? 20 : 45;
-        u16 chanceNotRare = itemHeldBoost ? 80 : 95;
+        u16 chanceNotRare = itemHeldBoost ? 70 : 90;
 
         for (i = 0; i < count; i++)
         {
